83ed7c143d6f9baf9ee1142b5f1209fbfe57a1ba
jhlzzz <98371537+jhlzzz@users.noreply.github.com>
2022-02-25 12:24:04 +0900

    Add files via upload

---
+++ ChartController.java
@@ -0,0 +1,38 @@
+package soo.md.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import soo.md.domain.ChartDTO;
+
+@Controller
+@RequestMapping("chart")
+public class ChartController {
+	@GetMapping("chart.do")
+	public String showView() {
+		return "chart/chart";
+	}
+	
+	private Random r = new Random();
+	@ResponseBody
+	@PostMapping("chartData")
+	public List<ChartDTO> getChartData(){
+		List<ChartDTO> list = new ArrayList<ChartDTO>();
+		String items[] = {"봄","여름","가을","겨울"};
+		
+		for(int i=0;i<items.length;i++) {
+			int number = r.nextInt(100);
+			ChartDTO dto = new ChartDTO(items[i], number);
+			list.add(dto);
+		}
+		return list;
+			
+	}
+}
